9b7a16a8ca2d9d5e15b5fd107d065a2e6027333d
Sara Hender <pinkpegasus@gmail.com>
2013-01-15 22:22:22 -0800

    tidy up!

--- App_Code/Handler.cs
+++ App_Code/Handler.cs
@@ -36,32 +36,19 @@ public class Handler : IHttpHandler
         HttpRequest Request = context.Request;
         HttpResponse Response = context.Response;
 
-        //if (Request.ContentType == "text/xml")
+        // Update Check, Ping Request, Event Check
+        // positive
+        // negative
+        if ("https://client.vitruvian.biz/update2" == Request.Url.ToString() || "http://client.vitruvian.biz/update2" == Request.Url.ToString())
         {
-
-            //System.Net.HttpWebRequest myRequest;
-
             Response.ContentType = "text/xml";
             StringBuilder sb = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
             sb.Append("<response protocol=\"3.0\" server=\"prod\">");
-
-            // parse request
-            //Request.ContentType == 
-            //Request.ContentLength == 
-            //Request.RequestType == 
-            //Request.Url ==
-            //Request.Method ==
-            //Request.IsSecureConnection
-            //Request.QueryString
-            //Request.RequestContext
 
             Stream stream = Request.InputStream;
             StreamReader reader = new StreamReader(stream, Request.ContentEncoding);
             string text = reader.ReadToEnd();
 
-            System.Diagnostics.Debug.WriteLine(text);
-
-            //sb.Append(text);
             StringBuilder output = new StringBuilder();
             StringBuilder version = new StringBuilder();
             StringBuilder appId = new StringBuilder();
@@ -96,23 +83,17 @@ public class Handler : IHttpHandler
                                         xmlReader.MoveToAttribute(i);
                                         if (xmlReader.Name == "appid")
                                         {
-                                            // is wrapped in { }
-                                            //Console.WriteLine(" {0}={1}", xmlReader.Name, xmlReader.Value);
                                             appId.Append(xmlReader.Value);
                                         }
                                         if (xmlReader.Name == "version")
                                         {
-                                            //Console.WriteLine(" {0}={1}", xmlReader.Name, xmlReader.Value);
-                                            //sb.Append(xmlReader.Value);
                                         }
                                     }
                                     xmlReader.MoveToElement();
                                 }
-                                //sb.Append(xmlReader.Name);
                                 writer.WriteStartElement(xmlReader.Name);
                                 break;
                             case XmlNodeType.Text:
-                                //sb.Append(xmlReader.Value);
                                 writer.WriteString(xmlReader.Value);
                                 break;
                             case XmlNodeType.XmlDeclaration:
@@ -130,89 +111,52 @@ public class Handler : IHttpHandler
                 }
             }
 
-            // Update Check, Ping Request, Event Check
-            // positive
-            // negative
-            if ("https://client.vitruvian.biz/update2" == Request.Url.ToString() || "http://client.vitruvian.biz/update2" == Request.Url.ToString())
+            // number of seconds since midnight
+            int secs = (int)System.DateTime.Now.TimeOfDay.TotalSeconds;
+
+            sb.Append("<daystart elapsed_seconds=\"" + secs.ToString() + "\"/>");
+            sb.Append("<app appid=\"" + appId.ToString() + "\" status=\"ok\">");
+
+            if (isUpdateCheck)
             {
-                // number of seconds since midnight
-                int secs = (int)System.DateTime.Now.TimeOfDay.TotalSeconds;
-
-                sb.Append("<daystart elapsed_seconds=\"" + secs.ToString() + "\"/>");
-                sb.Append("<app appid=\"" + appId.ToString() + "\" status=\"ok\">");
-
-                //sb.Append(Request.RequestContext.ToString());
-                if (isUpdateCheck)
+                if (false)
                 {
-                    if (false)
-                    {
-                        // no update
-                        sb.Append("<updatecheck status=\"noupdate\"/>");
-                    }
-                    else
-                    {
-/*
- * <?xml version="1.0" encoding="UTF-8"?>
- * <response protocol="3.0" server="prod">
- * <daystart elapsed_seconds="42774"/>
- * <app appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" status="ok">
- * <updatecheck status="ok">
- * <urls>
- * <url codebase="http://cache.pack.google.com/edgedl/chrome/win/FF93E464A075D0E7/"/>
- * <url codebase="http://www.google.com/dl/chrome/win/FF93E464A075D0E7/"/>
- * <url codebase="https://dl.google.com/chrome/win/FF93E464A075D0E7/"/>
- * <url codebase="http://dl.google.com/chrome/win/FF93E464A075D0E7/"/>
- * <url codebase="http://google.com/dl/chrome/win/FF93E464A075D0E7/"/>
- * </urls>
- * <manifest version="23.0.1271.97">
- * <packages>
- * <package hash="vvRHaljhwCg1XFC1WVKQKOxYk0E=" name="23.0.1271.97_chrome_installer.exe" required="true" size="30646264"/>
- * </packages>
- * <actions>
- * <action arguments="--multi-install --chrome --verbose-logging --do-not-launch-chrome" event="install" run="23.0.1271.97_chrome_installer.exe"/>
- * <action Version="23.0.1271.97" event="postinstall" onsuccess="exitsilentlyonlaunchcmd"/>
- * </actions>
- * </manifest>
- * </updatecheck>
- * </app>
- * </response>
- */
-// wants sha1 hash
-//"aa42eeb11c54cf9cacd2385ea1c3c7974826ca1a"
-
-                        sb.Append("<updatecheck status=\"ok\">");
-                        sb.Append("<urls>");
-                        sb.Append("<url codebase=\"http://dl.vitruvian.biz/\"/>");
-                        sb.Append("</urls>");
-                        sb.Append("<manifest version=\"1.5.0\">");
-                        sb.Append("<packages>");
-                        sb.Append("<package hash=\"qkLusRxUz5ys0jheocPHl0gmyho=\" name=\"infoatoms-setup.exe\" required=\"true\" size=\"1195336\"/>");
-                        sb.Append("</packages>");
-                        sb.Append("<actions>");
-                        sb.Append("<action arguments=\"\" event=\"install\" run=\"infoatoms-setup.exe\"/>");
-                        sb.Append("<action version=\"1.5.0\" event=\"postinstall\" onsuccess=\"exitsilentlyonlaunchcmd\"/>");
-                        sb.Append("</actions>");
-                        sb.Append("</manifest>");
-                        sb.Append("</updatecheck>");
-                    }
-                    //sb.Append("<ping status=\"ok\"/>");
+                    // no update
+                    sb.Append("<updatecheck status=\"noupdate\"/>");
                 }
-                else if (isEventReport)
+                else
                 {
-                    sb.Append("<event status=\"ok\"/>");
+                    // wants sha1 hash
+                    //"aa42eeb11c54cf9cacd2385ea1c3c7974826ca1a"
+
+                    sb.Append("<updatecheck status=\"ok\">");
+                    sb.Append("<urls>");
+                    sb.Append("<url codebase=\"http://dl.vitruvian.biz/\"/>");
+                    sb.Append("</urls>");
+                    sb.Append("<manifest version=\"1.5.0.0\">");
+                    sb.Append("<packages>");
+                    sb.Append("<package hash=\"qkLusRxUz5ys0jheocPHl0gmyho=\" name=\"infoatoms-setup.exe\" required=\"true\" size=\"1195336\"/>");
+                    sb.Append("</packages>");
+                    sb.Append("<actions>");
+                    sb.Append("<action arguments=\"\" event=\"install\" run=\"infoatoms-setup.exe\"/>");
+                    sb.Append("<action version=\"1.5.0.0\" event=\"postinstall\" onsuccess=\"exitsilentlyonlaunchcmd\"/>");
+                    sb.Append("</actions>");
+                    sb.Append("</manifest>");
+                    sb.Append("</updatecheck>");
                 }
-                sb.Append("</app>");
             }
-
-            // Event Report
-            // can just respond 200 for now
-
-            // Data Request
-            //
-
+            else if (isEventReport)
+            {
+                sb.Append("<event status=\"ok\"/>");
+            }
+            sb.Append("</app>");
             sb.Append("</response>");
             Response.Write(sb);
-            //Response.Write(text);
+        }
+        //for the other calls, check2, etc we don't know what they want, the best we can do is not throw a server error
+        else
+        {
+
         }
     }
     public bool IsReusable
